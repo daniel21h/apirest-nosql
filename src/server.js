c29af56fc5940c01eeccb8a4292673f3cdb5e96d
daniel21h <hesseldaniel21@gmail.com>
2020-09-05 11:46:32 -0300

    feat: relations models and add middlewares

--- src/server.js
+++ src/server.js
@@ -5,8 +5,8 @@ const server = express()
 server.use(express.json())
 server.use(express.urlencoded({ extended: false }))
 
-require('./controllers/authController')(server)
-require('./controllers/projectController')(server)
+require('./server/controllers/authController')(server)
+require('./server/controllers/projectController')(server)
 
 server.listen(3333, () => {
   console.log('Server running on port 3333')
