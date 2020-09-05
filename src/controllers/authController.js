26acff219d195abb5b6b82b3276518ed2874eb74
daniel21h <hesseldaniel21@gmail.com>
2020-09-04 22:11:52 -0300

    feat: create user in DB

---
+++ src/controllers/authController.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const User = require('../models/User')
+
+const router = express.Router()
+
+router.post('/register', async (request, response) => {
+  try {
+    const user = await User.create(request.body)
+
+    return response.json({ user })
+  } catch (err) {
+    return response.status(400).send({ error: 'Registration failed.' })
+  }
+})
+
+module.exports = router
--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ const server = express()
 server.use(express.json())
 server.use(express.urlencoded({ extended: false }))
 
+server.use(require('./controllers/authController'))
+
 server.listen(3334, () => {
   console.log('Server running on port 3334')
 })
